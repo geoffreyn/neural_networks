ca74f22d36e804c32f65d578727243dd825b8c07
Geoffrey Newman <geoffrey.newman@qeexo.com>
2019-06-16 17:35:25 +0800

    Save autoencoded images to video for efficiency
    * Add sys argument to skip training adjustment to neural-net

--- mnist_autoencoder.py
+++ mnist_autoencoder.py
@@ -9,6 +9,7 @@ from keras.datasets import mnist
 from keras.layers import Dense, Flatten, Dropout
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
+from matplotlib import animation
 import matplotlib.pylab as plt
 import numpy as np
 
@@ -28,7 +29,11 @@ flatx_test = np.reshape(x_test,
 flaty_test = np.squeeze(flatx_test)
 
 
-def main():
+def main(argv):
+
+    if len(argv) > 0:
+        skip = int(argv[0])
+
     model = Sequential()
     model.add(Flatten(input_shape=input_shape))
     model.add(Dense(1000, activation='relu'))
@@ -47,34 +52,58 @@ def main():
     if os.path.exists('model/autoencoder.hdf'):
         print('Model exists, loading...')
         model.load_weights('model/autoencoder.hdf')
+    else:
+        skip=False
 
-    history = AccuracyHistory()
+    if not skip:
+        history = AccuracyHistory()
 
-    model.fit(x_train, flaty_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=1,
-              validation_data=(x_test, flaty_test),
-              callbacks=[history, early_stopping_monitor])
-    score = model.evaluate(x_test, flaty_test, verbose=0)
-    plt.plot(history.acc)
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.show()
-    model.save_weights('model/autoencoder.hdf')
+        model.fit(x_train, flaty_train,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  verbose=1,
+                  validation_data=(x_test, flaty_test),
+                  callbacks=[history, early_stopping_monitor])
+        score = model.evaluate(x_test, flaty_test, verbose=0)
+        plt.plot(history.acc)
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.show()
+        model.save_weights('model/autoencoder.hdf')
 
     os.makedirs('results/images/', exist_ok=True)
 
     plt.set_cmap('gray')
-    fig, ax = plt.subplots(1,2)
-    for num in range(x_train.shape[0]):
-        ax[0].imshow(np.squeeze(x_train[num:num+1,:,:,:]))
 
-        ax[1].imshow(np.reshape(model.predict(x_train[num:num+1,:,:,:]),
+    fig, ax = plt.subplots(1, 2)
+
+    img = [ax[0].imshow(np.zeros((28, 28)), vmin=0, vmax=1),
+           ax[1].imshow(np.zeros((28, 28)), vmin=0, vmax=1)]
+
+    ax[0].set_title('Original')
+    ax[1].set_title('Reconstructed')
+
+    def init():
+        img[0].set_data(np.zeros((28, 28)))
+        img[1].set_data(np.zeros((28, 28)))
+
+        return (img,)
+
+    def animate(num):
+        img[0].set_data(np.squeeze(x_train[num:num+1,:,:,:]))
+
+        img[1].set_data(np.reshape(model.predict(x_train[num:num+1,:,:,:]),
                                 (x_train.shape[1], x_train.shape[2])))
 
-        fig.savefig('results/images/ae_{num}.png'.format(num=num))
+        return (img,)
+
+    # call the animator. blit=True means only re-draw the parts that have
+    # changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=100, interval=200, blit=False)
+
+    anim.save('results/images/autoencoder.mp4')
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
